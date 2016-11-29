9ffd52b163a373080baee578257d1606d3eb0cbf
Amey Parundekar <parundekaramey@gmail.com>
2016-11-29 23:17:14 +0530

    Update jqueryindex.js
    Added animate api

--- ui/jqueryindex.js
+++ ui/jqueryindex.js
@@ -8,20 +8,3 @@ $(document).ready(function(){
 
 
 });
-=======
-$(document).ready(function(){
-
-	setInterval(function(){
-		if($('blink').css('visibility')=="hidden"){
-			$('blink').css('visibility',"");
-		}else{
-			$('blink').css("visibility","hidden");
-		}
-	},500);
-
-			$('#login').click(function(){
-				$('#text').load("login.html #text");
-			});
-
-});
->>>>>>> 2f12b4e36d88d5797a0247a554623c75530fde7c
