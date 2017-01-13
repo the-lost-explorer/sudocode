ef72c0d38d8bce433d7544e4328ea339cf001acd
AMEY PARUNDEKAR <AMEY PARUNDEKAR>
2017-01-14 00:55:30 +0530

    made_seperate_categories_page

--- ui/dash.js
+++ ui/dash.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   $('#text').height($(window).height()-($('#header').height()+$('#footer').height()));
-  //Get Username
+
+
+
+//Get Username
   var request_username = new XMLHttpRequest();
   request_username.onload = function(){
     console.log('done1')
@@ -30,61 +33,7 @@ $('#dropdown').click(function(){
 });
 
 
-
-//Categories Request
-
-$('#categories').click(function(){
-
-  var request_categories = new XMLHttpRequest();
-  request_categories.onload = function(){
-
-      if(request_categories.readystate = XMLHttpRequest.DONE){
-
-
-        if(request_categories.status===200||request_categories.status===304){
-            var data = JSON.parse(request_categories.responseText);
-            $('#dashbody').html('');
-            for(var x=0;x<data.length;x++){
-              $('#dashbody').append('<div class="categories_tab" id="temp_id"><span>'+'#'+data[x]+'</span></div>');
-              $('#temp_id').attr('id',data[x]);
-            }
-            $('#categories').remove();
-            $('#dashbody').append('<br>');
-            //$('#post').remove();
-        }
-
-        else if(request_categories.status===500){
-
-          $('#dashbody').append('<div/>',{
-            id: 'error',
-            text: 'Error loading categories, try again later!'
-          });
-
-        }
-
-        else if(request_categories.status===403){
-          $('#dashbody').append('<div/>',{
-            id:'login_error',
-            text: 'Lol you didn\'t login'
-          });
-        }
-      }
-
-
-
-
-
-  }
-
-  request_categories.open('GET','http://localhost:8082/get-categories/',true);
-  request_categories.send(null);
-
-
-});
-
-
-
-
+//Categories Page opening request
 $(this).on("click", ".categories_tab", function(){
     console.log('clicked_categories');
     console.log(this.id);
@@ -113,6 +62,8 @@ $(this).on("click", ".categories_tab", function(){
     });
 
 
+
+//Categories inside post
   categories = [];
 
 $('#postcategory').click(function(){
@@ -176,16 +127,9 @@ $('#postcategory').click(function(){
 
 });
 
-
-
-//Create Article
-
-
-
-
-
-
-
+$("#categories").click(function(){
+  window.location.href = "http://localhost:8082/ui/categories.html";
+})
 
 
 setInterval(function(){
